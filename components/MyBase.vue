534e0fac7de9ad36ae19dbaebdfdec030bc69e14
Luyen Tran <tranminhluyen.1993@gmail.com>
2022-12-07 23:44:04 +0700

    recusive magic

--- components/MyBase.vue
+++ components/MyBase.vue
@@ -1,29 +1,51 @@
 <template>
-  <div class="w-full h-full p-1 w-full">
-    <draggable
-      :list="props.item.items"
-      :group="{ name: 'g1' }"
-      item-key="name">
-      <template #item="{ element, index }">
-        <div class="rounded-md p-1 my-1 cursor-pointer border" :class="(deep % 2) ? 'bg-gray-100' : 'bg-gray-300'">
-          <div class="flex justify-between">
-            <div class="font-bold px-2 cursor-pointer"> {{ element.name || element }}</div>
-            <div class="flex space-x-1">
-              <div class="bg-white shadow hover:bg-gray-100 px-2 cursor-pointer" @click.self="addElement(element)">add</div>
-              <div class="bg-white shadow hover:bg-gray-100 px-2 cursor-pointer" @click.self="delElement(index)">del</div>
-            </div>
+  <draggable
+    :list="props.items"
+    :group="{ name: 'g1' }"
+    handle=".handle"
+    tag="ul"
+    item-key="id">
+    <template #item="{ element, index }">
+      <li class="rounded shadow " :class="(deep % 2) ? 'bg-gray-100' : 'bg-gray-300'">
+        <!-- header -->
+        <div class="flex justify-between items-center bg-gray-600">
+          <div class="flex">
+            <div class="w-6 h-8 bg-blue-500 handle cursor-pointer"></div>
+            <input type="text" v-model="element.name" class="px1 m1 w-30">
           </div>
-          <MyBase v-if="element.items.length" :item="element" :deep="(props.deep + 1)"></MyBase>
+          <div class="flex space-x-1 px1">
+            <select v-if="(element.items.length > 1)" v-model="element.col">
+              <option v-for="col in list_col">{{ col }}</option>
+            </select>
+            <div class="bg-white shadow hover:bg-gray-100 px-2 cursor-pointer" @click.self="addElement(element)">+</div>
+            <div class="bg-white shadow  hover:bg-gray-100 px-2 cursor-pointer" @click.self="delElement(index)">-</div>
+          </div>
         </div>
-      </template>
-    </draggable>
-  </div>
+        <!-- content -->
+        <MyBase class="p1 grid gap-1" :class="element.col" v-if="element.items.length" :items="element.items" :deep="(props.deep + 1)"></MyBase>
+        <div v-else class="w-full flex justify-center p1">
+          <img class="object-contain" src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2021/11/10/972631/Taylor-Swift-Enchant.jpg" alt="">
+        </div>
+      </li>
+    </template>
+  </draggable>
 </template>
 <script setup>
 import draggable from 'vuedraggable'
+const list_col = [
+  "grid-cols-1",
+  "grid-cols-2",
+  "grid-cols-3",
+  "grid-cols-4",
+  "grid-cols-5",
+  "grid-cols-6",
+  "grid-cols-7",
+  "grid-cols-8",
+
+]
 const props = defineProps({
-  item: {
-    type: Object
+  items: {
+    type: Array
   },
   deep: {
     type: Number
@@ -33,10 +55,11 @@ const props = defineProps({
 function addElement(element) {
   element.items.push({
     name: element.items.length + '',
-    items: []
+    items: [],
+    col: "grid-cols-2",
   },)
 }
 function delElement(index) {
-  props.item.items.splice(index, 1);
+  props.items.splice(index, 1);
 }
 </script>
--- pages/builder.vue
+++ pages/builder.vue
@@ -3,59 +3,11 @@
     <div class="h-full flex flex-col border-r border-black min-w-20">
       <div>MyBase</div>
     </div>
-    <div class="flex-1 p-3 overflow-y-scroll">
-      <MyBase :item="item" :deep="0"></MyBase>
+    <div class="flex-1 overflow-y-scroll">
+      <MyBase :items="item" :deep="0"></MyBase>
     </div>
   </div>
 </template>
 <script setup>
-const item = ref(
-  {
-    name: "Phones",
-    items: [
-      {
-        name: "Iphone",
-        items: [
-          {
-            name: "IPhone 3",
-            items: [
-              {
-                name: "X",
-                items: []
-              },
-              {
-                name: "XS",
-                items: []
-              }
-            ]
-          },
-          {
-            name: "IPhone 4",
-            items: [
-              {
-                name: "X",
-                items: []
-              },
-              {
-                name: "XS",
-                items: []
-              }
-            ]
-          }, {
-            name: "IPhone 5",
-            items: [{
-              name: "X",
-              items: []
-            },
-            {
-              name: "XS",
-              items: []
-            }
-            ]
-          }
-        ]
-      },
-    ]
-  },
-)
+const item = ref([{ "name": "1", "items": [], "col": "grid-cols-2" }])
 </script>
